b2e2e294f44c435133ec997c10982b62a839d681
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2024-03-07 10:53:51 -0600

    adding StreamLayer

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -12,25 +12,32 @@ import (
 var _ raft.FSM = (*fsm)(nil)
 var _ raft.FSMSnapshot = (*snapshot)(nil)
 var _ raft.LogStore = (*logStore)(nil)
+var _ raft.StreamLayer = (*StreamLayer)(nil)
 
 const (
 	AppendRequestType RequestType = 0
 )
 
+type DistributedLog struct {
+	config Config
+	log    *Log
+	raft   *raft.Raft
+}
+
 type fsm struct {
 	log *Log
 }
+
+type RequestType uint8 
 
 type snapshot struct {
 	reader io.Reader
 }
 
-type RequestType uint8 
-
-type DistributedLog struct {
-	config Config
-	log    *Log
-	raft   *raft.Raft
+type StreamLayer struct {
+	ln net.Listener
+	serverTLSConfig *tls.Config
+	peerTLSConfig *tls.Config
 }
 
 func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
@@ -321,4 +328,69 @@ func (l *logStore) StoreLogs(records []*raft.Log) error {
 		}
 	}
 	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
+}
+
+func NewStreamLayer(
+	ln net.Listener,
+	serverTLSConfig,
+	peerTLSConfig *tls.Config,
+) *StreamLayer {
+	return &StreamLayer{
+		ln: ln,
+		serverTLSConfig: serverTLSConfig,
+		peerTLSConfig: peerTLSConfig,
+	}
+}
+
+const RaftRPC = 1
+ 	
+func (s *StreamLayer) Dial(
+		addr raft.ServerAddress,
+		timeout time.Duration,
+) (net.Conn, error) {
+		dialer := &net.Dialer{Timeout: timeout}
+		var conn, err = dialer.Dial("tcp", string(addr))
+		if err != nil {
+				return nil, err
+		}
+		// identify to mux this is a raft rpc
+		_, err = conn.Write([]byte{byte(RaftRPC)})
+		if err != nil {
+				return nil, err
+		}
+		if s.peerTLSConfig != nil {
+				conn = tls.Client(conn, s.peerTLSConfig)
+		}
+		return conn, err
+}
+
+func (s *StreamLayer) Accept() (net.Conn, error) {
+	conn, err := s.ln.Accept()
+	if err != nil {
+			return nil, err
+	}
+	b := make([]byte, 1)
+	_, err = conn.Read(b)
+	if err != nil {
+			return nil, err
+	}
+	if bytes.Compare([]byte{byte(RaftRPC)}, b) != 0 {
+			return nil, fmt.Errorf("not a raft rpc")
+	}
+	if s.serverTLSConfig != nil {
+			return tls.Server(conn, s.serverTLSConfig), nil
+	}
+	return conn, nil
+}
+
+func (s *StreamLayer) Close() error {
+	return s.ln.Close()
+}
+
+func (s *StreamLayer) Addr() net.Addr {
+	return s.ln.Addr()
 }
