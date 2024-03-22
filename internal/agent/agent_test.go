bf91cd05a0223a82fa69213c59f1e4778ad8955f
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2024-03-22 09:43:34 -0500

    updating end-to-end tests in agent_test.go

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -18,6 +18,7 @@ import (
 	api "github.com/whitneylampkin/proglog/api/v1"
 	"github.com/whitneylampkin/proglog/internal/agent"
 	"github.com/whitneylampkin/proglog/internal/config"
+	"github.com/whitneylampkin/proglog/internal/loadbalance"
 )
 
 func TestAgent(t *testing.T) {
@@ -93,6 +94,10 @@ func TestAgent(t *testing.T) {
 		},
 	)
 	require.NoError(t, err)
+
+	// wait until replication has finished
+	time.Sleep(3 * time.Second)
+
 	consumeResponse, err := leaderClient.Consume(
 		context.Background(),
 		&api.ConsumeRequest{
@@ -101,8 +106,6 @@ func TestAgent(t *testing.T) {
 	)
 	require.NoError(t, err)
 	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
-	// wait until replication has finished
-	time.Sleep(3 * time.Second)
 
 	followerClient := client(t, agents[1], peerTLSConfig)
 	consumeResponse, err = followerClient.Consume(
@@ -137,7 +140,8 @@ func client(
 	rpcAddr, err := agent.Config.RPCAddr()
 	require.NoError(t, err)
 	conn, err := grpc.Dial(fmt.Sprintf(
-		"%s",
+		"%s:///%s",
+		loadbalance.Name,
 		rpcAddr,
 	), opts...)
 	require.NoError(t, err)
